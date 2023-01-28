538fde1154bc9cf1d5543e7fbd38f88738a3fc15
sinjaskr <123108815+sinjaskr@users.noreply.github.com>
2023-01-28 16:17:48 -0500

    Update CardTrick.java

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -57,7 +57,7 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Jaskaran Singh, but you can call me Jaskaran");
         System.out.println();
         
         System.out.println("My career ambitions:");
